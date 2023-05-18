57b645be80abdee6ff380b0b2c359f8a6b7c5c8a
Alfonso <almoma.info@gmail.com>
2023-05-18 12:18:29 +0200

    Version buena

---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
+import { CacheStore } from '../../interfaces/cache-store.interface';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [
+  ]
+})
+export class ByCountryPageComponent implements OnInit{
+
+  countries:Country[] = [];
+
+  public initialValue:string = '';
+
+  constructor(private countriesService:CountriesService){
+
+  }
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+  }
+
+  searchByCountry(term:string){
+    this.countriesService.searchCountry(term)
+    .subscribe( countries => this.countries = countries)
+  }
+
+}
